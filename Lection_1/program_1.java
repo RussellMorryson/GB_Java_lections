edc62f6b4a29afb19844330d65febcdcba2e3d91
RussellMorryson <118932212+RussellMorryson@users.noreply.github.com>
2023-04-13 14:09:41 +0000

    Add code

---
+++ Lection_1/program_1.java
@@ -0,0 +1,9 @@
+public class program_1 {
+    static public void main (String[] args) {
+        System.out.println("Hello!");
+
+        int a = 123;
+        a = --a-a--;
+        System.out.println(a);
+    }
+}
